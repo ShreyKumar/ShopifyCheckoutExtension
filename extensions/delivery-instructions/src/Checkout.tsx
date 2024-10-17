11b94888bd6fc1797ce7d7b940bd7ca3622408cc
Shrey Kumar <shreykumar961@gmail.com>
2024-10-16 20:45:19 -0400

    AInitial commit

---
+++ extensions/delivery-instructions/src/Checkout.tsx
@@ -0,0 +1,54 @@
+import {
+  reactExtension,
+  Banner,
+  BlockStack,
+  Checkbox,
+  Text,
+  useApi,
+  useApplyAttributeChange,
+  useInstructions,
+  useTranslate,
+} from "@shopify/ui-extensions-react/checkout";
+
+// 1. Choose an extension target
+export default reactExtension("purchase.checkout.delivery-address.render-after", () => (
+  <Extension />
+));
+
+function Extension() {
+  const translate = useTranslate();
+  const { extension } = useApi();
+  const instructions = useInstructions();
+  const applyAttributeChange = useApplyAttributeChange();
+
+
+  // 2. Check instructions for feature availability, see https://shopify.dev/docs/api/checkout-ui-extensions/apis/cart-instructions for details
+  if (!instructions.attributes.canUpdateAttributes) {
+    // For checkouts such as draft order invoices, cart attributes may not be allowed
+    // Consider rendering a fallback UI or nothing at all, if the feature is unavailable
+    return (
+      <Banner title="delivery-instructions" status="warning">
+        {translate("attributeChangesAreNotSupported")}
+      </Banner>
+    );
+  }
+
+  // 3. Render a UI
+  return (
+    <BlockStack border={"dotted"} padding={"tight"}>
+      <Checkbox onChange={onCheckboxChange}>
+        {translate("track")}
+      </Checkbox>
+    </BlockStack>
+  );
+
+  async function onCheckboxChange(isChecked) {
+    // 4. Call the API to modify checkout
+    const result = await applyAttributeChange({
+      key: "advancedTracker",
+      type: "updateAttribute",
+      value: isChecked,
+    });
+    console.log("applyAttributeChange result", result);
+  }
+}
